ab9ab7b2e4aa09339f46f8d04d0a694045a494e1
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-08-26 23:42:26 -0300

    documentei o js e adicionei a função para calcular porcentagem

--- assets/src/script/index.js
+++ assets/src/script/index.js
@@ -1,23 +1,41 @@
+//seleciona a tag p que tem uma div response
 let response = document.querySelector('#response');
-        
-function toClean() {
-    response.innerHTML = "";
-}
 
+//essa função adiciona um numeros     
 function toAdd(num) {
     response.innerHTML += num;
 }
 
+//essa função limpa tudo
+function toClean() {
+    response.innerHTML = "";
+}
+
+//ela adiciona os operadores matematicos
 function operators(str) {
     response.innerHTML += str;
 }
 
+//essa função calcula tudo
 function calculate() {
     let input = response.innerHTML;
-    try {
-        let result = eval(input);
+
+    if (input.includes('%')) {
+        let parts = input.split('%');
+        if (parts.length === 2) {
+            let value = parseFloat(parts[0]);
+            let percent = parseFloat(parts[1]);
+            if (!isNaN(value) && !isNaN(percent)) {
+                let result = (value * percent) / 100;
+                response.innerHTML = result;
+            }
+        }
+    response.innerHTML = "Erro";
+    }
+    try { 
+        let result = eval(input); //a função eval ela faz com que o html seja tratado como js
         response.innerHTML = result;
     } catch (error) {
         response.innerHTML = "Erro";
-    }
+    }   
 }
